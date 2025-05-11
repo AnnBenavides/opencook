a48a8e3aa6432e7cd5c8d783504779d378177e07
Ann <andrea.benavidesj@gmail.com>
2025-05-11 00:40:22 -0400

    could not find out folder of build

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
   basePath: process.env.BASE_URL || '',
