58a2e1d554423f2ca6fc310b01d1dfef1a14e155
Ann <andrea.benavidesj@gmail.com>
2025-05-10 23:46:26 -0400

    publish

---
+++ src/app/recipes/create/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {RecipeForm} from "@/components/Recipe/recipeForm";
+import {Base} from "@/components/Base";
+
+export default function CreateRecipes() {
+  return (
+    <Base>
+      <RecipeForm/>
+    </Base>
+    );
+}
